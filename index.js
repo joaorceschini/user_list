3db7aa448e5a50ca89ba3314e2dad93d07577ced
joaorceschini <jrvceschini@gmail.com>
2022-10-14 15:09:56 -0300

    ok

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 async function getContent() {
   try {
-    const response = await fetch('http://localhost:4567/')
-    const data = await response.json()
-    show(data)
+    const response = await fetch("http://localhost:4567/");
+    const data = await response.json();
+    show(data);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 }
 
-getContent()
+getContent();
 
 function show(users) {
-  let output = '';
+  let output = "";
 
   for (let user of users) {
-    output += `<li>${JSON.stringify(user)}</li>`
+    output += `
+      <li>
+        <img src="${user.picture.medium}" alt="User icon">
+        <div class="user_header">
+          <p class="name">${user.name.first} ${user.name.last}</p>
+          <p class="email">${user.email}</p>
+          <p class="gender">${user.gender}</p>
+        </div>
+      </li>
+    `;
   }
 
-  document.querySelector('.list').innerHTML = output;
+  document.querySelector(".list").innerHTML = output;
 }
